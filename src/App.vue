affdf156cbb8fc2669c0a531ab4e89f0c975dbde
Carlos Díaz <ernesto.diaz@finakiva.com>
2022-12-24 12:29:19 -0500

    :bricks: rename components and remove code dead

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <Navbar />
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
-  import HelloWorld from '@/components/HelloWorld.vue'
+  import Navbar from '@/components/Navbar.vue'
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Hola</h1>
+  <h2>Hola</h2>
+</template>
+
+<script setup lang="ts">
+</script>
