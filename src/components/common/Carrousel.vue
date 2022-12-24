a8419b995984acac0db2cd003fbb612d73b685d8
Carlos Díaz <ernesto.diaz@finakiva.com>
2022-12-24 18:08:00 -0500

    :sparkles: create component Carrousel

---
+++ src/components/common/Carrousel.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-carousel hide-delimiters>
+    <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src" cover>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  items: { type: Array, required: true },
+});
+</script>
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,39 +1,57 @@
 <template>
-  <Navbar />
-  <v-container class="mx-12">
-    <v-card-title class="text-center"><h2>Casos de exito</h2></v-card-title>
-    <Section
-      image-url="src/assets/example.jpg"
-      title="Seccion 1"
-      text="textio que nos interesa mostrar"
-      button-title="Algo 1"
-      :lambda="hacerAlgo"
-    />
-    <Section
-      image-url="src/assets/example.jpg"
-      title="Seccion 2"
-      text="textio que nos interesa mostrar"
-      button-title="Algo 2"
-      :lambda="hacerAlgo"
-    />
-    <Section
-      image-url="src/assets/example.jpg"
-      title="Seccion 3"
-      text="textio que nos interesa mostrar"
-      button-title="Algo 3"
-      :lambda="hacerAlgo"
-    />
-    <Section
-      image-url="src/assets/example.jpg"
-      title="Seccion 4"
-      text="textio que nos interesa mostrar"
-    />
+  <v-container>
+    <Navbar />
+    <Carrousel :items="items" />
+    <v-container class="mx-12">
+      <v-card-title class="text-center"><h2>Casos de exito</h2></v-card-title>
+      <Section
+        image-url="src/assets/example.jpg"
+        title="Seccion 1"
+        text="textio que nos interesa mostrar"
+        button-title="Algo 1"
+        :lambda="hacerAlgo"
+      />
+      <Section
+        image-url="src/assets/example.jpg"
+        title="Seccion 2"
+        text="textio que nos interesa mostrar"
+        button-title="Algo 2"
+        :lambda="hacerAlgo"
+      />
+      <Section
+        image-url="src/assets/example.jpg"
+        title="Seccion 3"
+        text="textio que nos interesa mostrar"
+        button-title="Algo 3"
+        :lambda="hacerAlgo"
+      />
+      <Section
+        image-url="src/assets/example.jpg"
+        title="Seccion 4"
+        text="textio que nos interesa mostrar"
+      />
+    </v-container>
   </v-container>
 </template>
 
 <script setup lang="ts">
 import Navbar from "@/components/Navbar.vue";
 import Section from "@/components/common/Section";
+import Carrousel from "@/components/common/Carrousel.vue";
+const items = [
+  {
+    src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+  },
+  {
+    src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+  },
+  {
+    src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+  },
+  {
+    src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+  },
+];
 
 const hacerAlgo = () => {
   console.log("xdxdxd");
