afcfd6d4b0f0e831c830afd962d7bf69705f4aa0
Carlos Díaz <ernesto.diaz@finakiva.com>
2022-12-24 13:48:36 -0500

    :sparkles: add image for example and created component for show data more image demonstrative

--- src/components/common/Section.vue
+++ src/components/common/Section.vue
@@ -1,5 +1,27 @@
 <template>
-  <v-container> </v-container>
+  <v-container>
+    <v-row>
+      <v-col cols="6">
+        <v-card>
+          <v-img :src="imageUrl" />
+        </v-card>
+      </v-col>
+      <v-col cols="6">
+        <v-card-title>
+          {{title}}
+        </v-card-title>
+        <v-card-text>
+          {{text}}
+        </v-card-text>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const props = defineProps({
+  imageUrl: String,
+  title: String,
+  text: String
+})
+</script>
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,11 +1,25 @@
 <template>
   <Navbar />
+  <Section
+    image-url="src/assets/example.jpg"
+    title="Seccion 1"
+    text="textio que nos interesa mostrar"
+  />
+  <Section
+    image-url="src/assets/example.jpg"
+    title="Seccion 1"
+    text="textio que nos interesa mostrar"
+  />
+  <Section
+    image-url="src/assets/example.jpg"
+    title="Seccion 1"
+    text="textio que nos interesa mostrar"
+  />
 </template>
 
 <script setup lang="ts">
-import Navbar from "@/components/Navbar.vue"
+import Navbar from "@/components/Navbar.vue";
+import Section from "@/components/common/Section";
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
