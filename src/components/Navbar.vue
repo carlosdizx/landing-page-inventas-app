b5320261bc3470caf06c08c9f0e1722adaf6f5bb
Cristhian-Rosas <Dev.frontend@digiworld.com.co>
2023-01-02 22:21:57 -0500

    navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,28 @@
 <template>
-//TODO i need a navbar with buttons example https://www.mailerlite.com/templates/landing-pages/hobbies @lororock
+  //TODO i need a navbar with buttons example
+  https://www.mailerlite.com/templates/landing-pages/hobbies @lororock
+
+  <v-app-bar color="teal-darken-4">
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </template>
+
+    <v-app-bar-title>INVETNAS</v-app-bar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn icon>
+      <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+  </v-app-bar>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
